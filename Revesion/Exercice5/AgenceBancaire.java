af6dfe0bf19c4d0d719c4a94714854fc22ae14ca
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-06 22:54:06 +0100

    put ArrayList in this exercice ..

--- Revesion/Exercice5/AgenceBancaire.java
+++ Revesion/Exercice5/AgenceBancaire.java
@@ -1,32 +1,33 @@
+import java.util.ArrayList;
+
 public class AgenceBancaire {
   private String agenceName;
   private int nbEffClient;
-  private final int max;
-  private Client[] clientArray;
+  private ArrayList <Client> clientArray = new ArrayList<Client>();
 
 
-  public AgenceBancaire(String agenceName, int max) {
+  public AgenceBancaire(String agenceName) {
     this.agenceName = agenceName;
-    this.max = max;
-    this.clientArray = new Client[max];
   }
 
   public void addClient(Client newClient) {
-      this.clientArray[nbEffClient++] = newClient;
+      this.clientArray.add(0, newClient);
+
   }
 
   public Client getClient(Identite clientIdentity) {
-    for (int i = 0; i < nbEffClient; i++) {
-      if (clientArray[i].getIdentiteClt().comparer(clientIdentity)) {
-        return clientArray[i];
+    for (int i = 0; i < clientArray.size(); i++) {
+      if (clientArray.get(i).getIdentiteClt().comparer(clientIdentity)) {
+        return clientArray.get(i);
       }
     }
     return null;
   }
 
   public void stream() {
-    for (int i = 0; i < nbEffClient; i++) {
-      clientArray[i].stream();
+    for (int i = 0; i < clientArray.size(); i++) {
+      System.out.println(this.clientArray.indexOf(clientArray.get(i)));
+      clientArray.get(i).stream();
       System.out.println("------^------------^--------^----------------^-----------");
     }
   }
--- Revesion/Exercice5/Principal.java
+++ Revesion/Exercice5/Principal.java
@@ -1,16 +1,18 @@
 public class Principal {
   public static void main(String[] args) {
-    AgenceBancaire twoOTw = new AgenceBancaire("2012", 60);
+    AgenceBancaire twoOTw = new AgenceBancaire("2012");
 
     PersonneID hamma = new PersonneID("Mohamed", "Bahri");
     PersonneID mouna = new PersonneID("Mouna", "jouini");
+    PersonneID belha = new PersonneID("Belhassen", "Gharsallah");
     EnterpriseID charika = new EnterpriseID("SOTUCO SARL", mouna);
 
     Client hammeClient = new Client(hamma, "Bizerte");
     twoOTw.addClient(hammeClient);
     Client mounaClient = new Client(charika, "Tunis");
     twoOTw.addClient(mounaClient);
-
+    Client belhaClient = new Client(belha, "Tunis");
+    twoOTw.addClient(belhaClient);
 
     twoOTw.stream();
 
