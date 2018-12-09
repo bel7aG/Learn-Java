93cdf4fb5f2fc8dcc26839b17449bf46d01c6251
bel7aG <belhassen.gharsallah@isetrades.com>
2018-12-09 03:47:59 +0100

    Start javafx for class project

---
+++ JavaFX/1-basics/1-demo/App.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+import javafx.stage.Stage;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("ok");
+  }
+}
